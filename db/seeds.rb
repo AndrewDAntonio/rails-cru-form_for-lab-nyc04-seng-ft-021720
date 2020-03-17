d273161a35fbad3046d9c2b61f903207648c96cc
AndrewDAntonio <github email address>
2020-03-17 18:07:56 -0400

    Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+drake = Artist.create(name: "Drake", bio: "Here for a good time not a long time")
+rihanna = Artist.create(name: "Rhi-Rhi", bio: "Ended up in a hopeless database")
+britney = Artist.create(name: "Britney Spears", bio: "It's Britney bitch!")
+
+pop = Genre.create(name: "Pop")
+rap = Genre.create(name: "Rap")
+randb = Genre.create(name: "R&B")
+
+crazy = Song.create(artist_id:3, genre_id:1,name: "Crazy")
+umbrellea = Song.create(artist_id:2,genre_id:3,name: "Umbrella")
+headlines = Song.create(artist_id:1, genre_id:2, name: "Headlines")
+
